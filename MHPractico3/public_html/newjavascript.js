115257a13a246efe302320204f95b78a1604467a
mas205 <martinhargain@gmail.com>
2017-04-20 10:57:58 -0300

    Update newjavascript.js

--- MHPractico3/public_html/newjavascript.js
+++ MHPractico3/public_html/newjavascript.js
@@ -1,10 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-//$().ready(iniciar);
+$().ready(iniciar);
 var numS = 0;
 var numP = 0;
 var numE = 0;
@@ -13,18 +7,150 @@ var tot = 0;
 var totS = 0;
 var cant = 0;
 var sueMB = 99999999;
-////function iniciar() {
-//    $("#btnE9").click(procesarEj9);
-//    $("#btnE10").click(procesarEj10);
-//    //$("#btnE11").click(procesarEj11);
-//    $("#btnE12").click(procesarEj12);
-//    $("#btnE13").click(procesarEj13);
-//}
-$("#numC").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#btnE11").click();
+function iniciar() {
+    $("#btnE1").click(procesarEj1);
+    $("#btnE2").click(procesarEj2);
+    $("#btnE3").click(procesarEj3);
+    $("#btnE4").click(procesarEj4);
+    $("#btnE5").click(procesarEj5);
+    $("#btnE6").click(procesarEj6);
+    $("#btnE7").click(procesarEj7);
+    $("#btnE8").click(procesarEj8);
+    $("#btnE9").click(procesarEj9);
+    $("#btnE10").click(procesarEj10);
+    $("#btnE11").click(procesarEj11);
+    $("#btnE12").click(procesarEj12);
+    $("#btnE13").click(procesarEj13);
+    $("#btnE14").click(procesarEj14);
+    $("#btnE15").click(procesarEj15);
+}
+function procesarEj1(){
+    var num1=$("#num1").val();
+    if (isNaN(num1)){
+        $("#pRes1").html("Error, intente devuelta con números");
     }
-});
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1>0){
+            $("#pRes1").html("Su número es mayor a 0");
+        }
+        else if(nnum1<0){
+            $("#pRes1").html("Su número es menor a 0");
+        }
+        else{
+            $("#pRes1").html("Su número es 0");
+        }
+    }
+}
+function procesarEj2(){
+    var num1=$("#num2").val();
+    if (isNaN(num1)){
+        $("#pRes2").html("Error, intente devuelta con números");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1>10){
+            $("#pRes2").html("Su número es mayor que 10");
+        }
+        else{
+            $("#pRes2").html("");
+        }
+    }
+}
+function procesarEj3(){
+    var num1=$("#num3").val();
+    if (isNaN(num1)){
+        $("#pRes3").html("Error, intente devuelta con números");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1>20){
+            $("#pRes3").html("Su número es mayor que 20");
+        }
+        else{
+            $("#pRes3").html("Su número es igual o menor que 20");
+        }
+    }
+}
+function procesarEj4(){
+    var num1=$("#num4").val();
+    if (isNaN(num1)){
+        $("#pRes4").html("Error, intente devuelta con números");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1>0){
+            $("#pRes4").html(nnum1);
+        }
+        else{
+            nnum2=-nnum1;
+            $("#pRes4").html(nnum2);
+        }
+    }
+}
+function procesarEj5(){
+    var num1=$("#num5").val();
+    if (isNaN(num1)){
+        $("#pRes5").html("Error, intente devuelta con números");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1<20 && nnum1>10){
+            $("#pRes5").html("Su número está entre 10 y 20");
+        }
+        else{
+            $("#pRes5").html("Su número no está entre 10 y 20");
+        }
+    }
+}
+function procesarEj6(){
+    var num1=$("#num6").val();
+    if (isNaN(num1)){
+        $("#pRes6").html("Error, intente devuelta con números");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1 % 7 === 0 && nnum1 % 3 === 0){
+            $("#pRes6").html("Su número es múltiplo de 3 y 7 simultáneamente");
+        }
+        else{
+            $("#pRes6").html("Su número no es múltiplo de 3 y 7 simultáneamente");
+        }
+    }
+}
+function procesarEj7(){
+    var num1=$("#num7").val();
+    if (isNaN(num1)){
+        $("#pRes7").html("Error, intente devuelta con números");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1<-20 || nnum1>20){
+            $("#pRes7").html("CUMPLE");
+        }
+        else{
+            $("#pRes7").html("NO CUMPLE");
+        }
+    }
+}
+function procesarEj8(){
+    var num1=$("#num8").val();
+    if (isNaN(num1)){
+        $("#pRes8").html("Error, intente devuelta con números");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        if(nnum1>30){
+            $("#pRes8").html("Su número es mayor a 30");
+        }
+        else if(nnum1<10){
+            $("#pRes8").html("Su número es menor a 10");
+        }
+        else{
+            $("#pRes8").html("Su número esta entre 10 y 30");
+        }
+    }
+}
 function procesarEj9() {
     var dia = "";
     var tempe = "";
@@ -59,7 +185,7 @@ function procesarEj9() {
         $("#pRes9").html("Error de formato, uso letras en temperatura");
     } else {
         nnum1 = parseFloat(num1);
-        switch (nnum1) {
+        switch (true) {
             case nnum1 > 20:
                 tempe = "Ponerse ropa comoda";
                 break;
@@ -164,5 +290,41 @@ function procesarEj13(){
                 return $("#pRes13").html("Ese operador no es valido, seleccione 'S', 'R', 'D', 'M'");
         }
         $("#pRes13").html("Resultado: "+res);
+    }
+}
+function procesarEj14(){
+    txt1=$("#txt14").val();
+    switch (txt1) {
+        case "a":
+        case "A":
+        case "e":
+        case "E":
+        case "i":
+        case "I":
+        case "o":
+        case "O":
+        case "u":
+        case "U":
+            $("#pRes14").html(txt1 + " es una vocal");
+            break;
+        default:
+            return $("#pRes14").html(txt1 + " es es una vocal");           
+    }
+}
+function procesarEj15(){
+    var num1 = $("#num15").val();
+    var num2 = $("#num16").val();
+    if(isNaN(num1) || isNaN(num2)){
+        $("#pRes15").html("Intente devuelta con un número");
+    }
+    else{
+        nnum1=parseFloat(num1);
+        nnum2=parseFloat(num2);
+        if (nnum1%nnum2===0){
+            $("#pRes15").html(nnum1+" y "+nnum2+" son múltiplos");
+        }
+        else{
+            $("#pRes15").html(nnum1+" y "+nnum2+" no son múltiplos");
+        }
     }
 }
